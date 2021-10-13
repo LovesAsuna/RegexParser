0eb3947542fcaec4d3aa42fb0e858fcaa9e2c923
LovesAsuna <qq625924077@gmail.com>
2021-10-13 23:45:57 +0800

    :sparkles: feat(converter): fix dfa converter, add dfa minimize

--- src/main/kotlin/com/hyosakura/regexparser/automata/Converter.kt
+++ src/main/kotlin/com/hyosakura/regexparser/automata/Converter.kt
@@ -62,37 +62,126 @@ interface Converter {
                 var hasNew = false
                 for (c in availableChar) {
                     val transferWrapper = getTransferWrapper(wrapper as StateWrapper, c, automata)
-                    if (!builder.contains(transferWrapper)) {
-                        hasNew = true
-                        builder.addState(transferWrapper)
-                        if (transferWrapper.accept) {
-                            acceptStates.add(transferWrapper.id)
+                    if (transferWrapper != null) {
+                        if (!builder.contains(transferWrapper)) {
+                            hasNew = true
+                            builder.addState(transferWrapper)
+                            if (transferWrapper.accept) {
+                                acceptStates.add(transferWrapper.id)
+                            }
+                            queue.offer(transferWrapper)
                         }
-                        queue.offer(transferWrapper)
+                        builder.addEdge(wrapper.id, c, transferWrapper.id)
                     }
-                    builder.addEdge(wrapper.id, c, transferWrapper.id)
                 }
                 if (!hasNew) break
+            }
+            // self loop
+            while (queue.isNotEmpty()) {
+                val wrapper = queue.poll()
+                for (c in availableChar) {
+                    getTransferWrapper(wrapper as StateWrapper, c, automata)?.apply {
+                        builder.addEdge(wrapper.id, c, id)
+                    }
+                }
             }
             return builder.setAcceptStates(acceptStates).build()
         }
 
         override fun minimize(automata: AutoMata<State, Edge>): AutoMata<State, Edge> {
             // automata must be a DFA
-            val builder = AbstractBuilder.DFABuilder()
-            val transferTable = mutableMapOf<MutableMap<Set<Char?>, Boolean>, PriorityQueue<Int>>()
-            // construct the transfer table
-            for ((index, state) in automata.stateMap) {
-                val map = mutableMapOf<Set<Char?>, Boolean>()
-                for (edge in state.edges) {
-                    val transferState = automata.nextStates(state, edge.symbol).first() as AttachState
-                    map[edge.symbol] = transferState.accept
+            if (automata !is DFA) throw IllegalArgumentException("Automata must be a DFA")
+            val list: MutableList<Set<State>> = mutableListOf()
+            val accept: Set<State> = automata.acceptStates
+            val notAccept: Set<State> = automata.stateMap.values.subtract(accept)
+            accept.apply {
+                if (isNotEmpty()) {
+                    list.addAll(group(accept, automata))
                 }
-                transferTable.computeIfAbsent(map) { PriorityQueue() }.add(index)
             }
-            // start build minimize DFA
+            notAccept.apply {
+                if (isNotEmpty()) {
+                    list.addAll(group(notAccept, automata))
+                }
+            }
+            // merge similar states
+            fun merge(stateSet: Set<State>): State {
+                var minId = Int.MAX_VALUE
+                val idSet = stateSet.stream().map {
+                    minId = minId.coerceAtMost(it.id)
+                    it.id
+                }.collect(Collectors.toSet())
+                val iterator = automata.stateMap.values.iterator() as MutableIterator
+                while (iterator.hasNext()) {
+                    val state = iterator.next()
+                    for (edge in state.edges) {
+                        if (edge.next in idSet) {
+                            edge.next = minId
+                        }
+                    }
+                    // remove redundant state
+                    if (state.id != minId && state.id in idSet) {
+                        iterator.remove()
+                    }
+                }
+                return automata.getState(minId)
+            }
+            for (stateSet in list) {
+                if (stateSet.size > 1) {
+                    merge(stateSet)
+                }
+            }
+            return automata
+        }
 
-            return builder.setStartState(automata.startState.id).setAcceptStates(setOf(1)).build()
+
+        private fun group(stateSet: Set<State>, automata: AutoMata<State, Edge>): List<Set<State>> {
+            val list = mutableListOf<Set<State>>()
+            val availableChar = getAvailableChar(automata)
+            val queue: Queue<Set<State>> = LinkedList()
+            queue.offer(stateSet)
+            for (char in availableChar) {
+                for (i in 0 until queue.size) {
+                    val groupList = groupSingleChar(queue.poll(), automata, char)
+                    queue.addAll(groupList)
+                }
+            }
+            return list.also {
+                it.addAll(queue)
+            }
+        }
+
+        private fun groupSingleChar(
+            stateSet: Set<State>,
+            automata: AutoMata<State, Edge>,
+            char: Set<Char?>
+        ): List<Set<State>> {
+            val acceptSet = mutableSetOf<State>()
+            val nonAcceptSet = mutableSetOf<State>()
+            val emptySet = mutableSetOf<State>()
+            for (state in stateSet) {
+                val nextState = automata.nextStates(state, char).firstOrNull() as? AttachState
+                if (nextState != null) {
+                    if (nextState.accept) {
+                        acceptSet.add(state)
+                    } else {
+                        nonAcceptSet.add(state)
+                    }
+                } else {
+                    emptySet.add(state)
+                }
+            }
+            return mutableListOf<Set<State>>().also {
+                if (acceptSet.isNotEmpty()) {
+                    it.add(acceptSet)
+                }
+                if (nonAcceptSet.isNotEmpty()) {
+                    it.add(nonAcceptSet)
+                }
+                if (emptySet.isNotEmpty()) {
+                    it.add(emptySet)
+                }
+            }
         }
 
         private fun getAvailableChar(automata: AutoMata<State, Edge>): Set<Set<Char?>> {
@@ -105,12 +194,15 @@ interface Converter {
             }.collect(Collectors.toSet())
         }
 
-        private fun getTransferWrapper(wrapper: StateWrapper, symbol: Set<Char?>, automata: NFA): StateWrapper {
+        private fun getTransferWrapper(wrapper: StateWrapper, symbol: Set<Char?>, automata: NFA): StateWrapper? {
             val transferSet = mutableSetOf<State>()
             for (state in wrapper.states) {
                 for (c in symbol) {
                     transferSet.addAll(automata.getFreeStates(automata.nextStates(state, c).toMutableSet()))
                 }
+            }
+            if (transferSet.isEmpty()) {
+                return null
             }
             val id = reversedMap[transferSet]
             return if (id != null) {
